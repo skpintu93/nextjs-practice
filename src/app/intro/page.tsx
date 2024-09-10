e05534815db0731f23fba840f4305c13f8e111cc
Pintu kumar <skpintu93@gmail.com>
2024-09-10 23:30:27 +0530

    up

---
+++ src/app/intro/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function kuchbhi() {
+  return (
+    <div>Hello, I am nutan kumari</div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Home({params, searchParams}) {
   return (
     <div>
       <h1>Hello World</h1>
